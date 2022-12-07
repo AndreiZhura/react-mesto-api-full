45caa00a9be11b6d844b4fe5fb5c229fe9a45ca2
Andrew Zhuravlev <zhura2296@yandex.ru>
2022-12-07 16:31:59 +0300

    подправил CORS

--- backend/middlewares/protected.js
+++ backend/middlewares/protected.js
@@ -1,16 +1,26 @@
 // Массив доменов, с которых разрешены кросс-доменные запросы
-
+const allowedCors = [
+  'https://andreizhura.nomoredomains.club',
+  'https://api.andreizhura.nomoredomains.club',
+  'http://andreizhura.nomoredomains.club',
+  'http://api.andreizhura.nomoredomains.club',
+  'https://localhost:3000',
+  'http://localhost:3000',
+];
 // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
 const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
 const giveOneAdress = (req, res, next) => {
+  const { origin } = req.headers;
+  const requestHeaders = req.headers['access-control-request-headers'];
   const { method } = req;
   // сохраняем список заголовков исходного запроса
-  const requestHeaders = req.headers['access-control-request-headers'];
   // проверяем, что источник запроса есть среди разрешённых
 
   // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
-  res.header('Access-Control-Allow-Origin', '*');
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
 
   // Если это предварительный запрос, добавляем нужные заголовки
   if (method === 'OPTIONS') {
