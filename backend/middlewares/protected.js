cabae9aa7e49cfbf065b7e723a7e1bdb7a6d9232
Andrew Zhuravlev <zhura2296@yandex.ru>
2022-12-07 15:51:06 +0300

    подправил CORS '*'

--- backend/middlewares/protected.js
+++ backend/middlewares/protected.js
@@ -1,26 +1,17 @@
 // Массив доменов, с которых разрешены кросс-доменные запросы
-const allowedCors = [
-  'https://andreizhura.nomoredomains.club',
-  'https://api.andreizhura.nomoredomains.club',
-  'http://andreizhura.nomoredomains.club',
-  'http://api.andreizhura.nomoredomains.club',
-  'localhost:3000',
-];
 
 // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
 const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
 const giveOneAdress = (req, res, next) => {
-  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
-
   const { method } = req;
   // сохраняем список заголовков исходного запроса
   const requestHeaders = req.headers['access-control-request-headers'];
   // проверяем, что источник запроса есть среди разрешённых
-  if (allowedCors.includes(origin)) {
-    // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
-    res.header('Access-Control-Allow-Origin', '*');
-  }
+
+  // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
+  res.header('Access-Control-Allow-Origin', '*');
+
   // Если это предварительный запрос, добавляем нужные заголовки
   if (method === 'OPTIONS') {
     // разрешаем кросс-доменные запросы любых типов (по умолчанию)
