85ce537194e6ff10bbd8611cdc6f44ba46671534
Andrew Zhuravlev <zhura2296@yandex.ru>
2022-12-07 18:34:42 +0300

    cors

--- backend/app.js
+++ backend/app.js
@@ -21,8 +21,23 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // app.js
+const options = {
+  origin: [
+    'https://andreizhura.nomoredomains.club',
+    'https://api.andreizhura.nomoredomains.club',
+    'http://andreizhura.nomoredomains.club',
+    'http://api.andreizhura.nomoredomains.club',
+    'https://localhost:3000',
+    'http://localhost:3000',
+  ],
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
+  credentials: true,
+};
 
-app.use(cors()); // ПЕРВЫМ!
+app.use(cors(options)); // ПЕРВЫМ!
 
 app.use(requestLogger);
 // роуты, не требующие авторизации,
